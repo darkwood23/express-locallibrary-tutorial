5383308f2848c0c1a26fc5e216c106b8f121cc40
Dark Wood <anonymoushackerintheweb@gmail.com>
2023-12-24 14:27:07 +0545

    First version of application moved into GitHub

---
+++ models/author.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const {DateTime} = require('luxon')
+
+const Schema = mongoose.Schema
+
+const AuthorSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date }
+})
+
+AuthorSchema.virtual("name").get(function () {
+    let fullname = ""
+    if (this.first_name && this.family_name) {
+        fullname = `${this.family_name}, ${this.first_name}`
+    }
+    return fullname
+})
+
+AuthorSchema.virtual("url").get(function () {
+    return `/catalog/author/${this._id}`
+})
+
+AuthorSchema.virtual("lifespan").get(function () {
+    let birth_date = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+    let death_date = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+
+    if (death_date === "Invalid DateTime") {
+        death_date === "Unknown"
+    }
+    if (birth_date === "Invalid DateTime") {
+        birth_date = "Unknown"
+    } 
+
+
+    return `${birth_date} - ${death_date}`
+})
+
+module.exports = mongoose.model("Author", AuthorSchema)
